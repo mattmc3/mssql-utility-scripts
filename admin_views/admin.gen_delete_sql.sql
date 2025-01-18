ce633f31c35c6fa13110708abe257ebb02d6517c
mattmc3 <mattmc3@gmail.com>
2025-01-18 11:13:25 -0500

    Tighten up delete generator

--- admin_views/admin.gen_delete_sql.sql
+++ admin_views/admin.gen_delete_sql.sql
@@ -6,44 +6,26 @@ create or alter view admin.gen_delete_sql as
 --------------------------------------------------------------------------------
 select top 2147483647 *
 from (
-
--- delete from
-select it.object_id
-     , it.schema_name
-     , it.table_name
-     , it.table_type
-     , 200000000 as seq
-     , 'DELETE FROM ' + it.quoted_name as sqltxt
-  from admin.table_info it
- where it.table_type = 'TABLE'
-
--- where
-union all
-select it.object_id
-     , it.schema_name
-     , it.table_name
-     , it.table_type
-     , 300000000 as seq
-     , space(6) + 'WHERE <Search Conditions,,>' as sqltxt
-  from admin.table_info it
- where it.table_type = 'TABLE'
-
--- end
-union all
-select it.object_id
-     , it.schema_name
-     , it.table_name
-     , it.table_type
-     , 999999000 + n.num as seq
-     , case n.num
-         when 1 then 'GO'
-         when 2 then ''
-       end as sqltxt
-  from admin.table_info it
- cross apply (select 1 as num union
-              select 2) n
- where it.table_type = 'TABLE'
-
+    -- delete statements
+    select it.object_id
+         , it.schema_name
+         , it.table_name
+         , it.table_type
+         , 100000000 * n.num as seq
+         , case n.num
+             when 1 then 'DELETE FROM ' + it.quoted_name
+             when 2 then space(6) + 'WHERE <Search Conditions,,>'
+             when 3 then 'GO'
+             when 4 then ''
+           end as sqltxt
+      from admin.table_info it
+     cross apply (
+             select 1 as num union
+             select 2 union
+             select 3 union
+             select 4
+         ) n
+     where it.table_type = 'TABLE'
 ) a order by 4, 2, 3, 5
 
 go
