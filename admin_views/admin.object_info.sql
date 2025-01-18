86021170d01a7efbb23b4cde21933f5c093e76dd
mattmc3 <mattmc3@gmail.com>
2025-01-18 11:41:37 -0500

    Update object_info

--- admin_views/admin.object_info.sql
+++ admin_views/admin.object_info.sql
@@ -5,9 +5,10 @@ create or alter view admin.object_info as
 -- Description: enhanced version of sysobjects
 --------------------------------------------------------------------------------
 select so.object_id
-     , so.name as object_name
      , so.schema_id
      , schema_name(so.schema_id) as schema_name
+     , so.name as object_name
+     , convert(nvarchar(1000), quotename(schema_name(so.schema_id)) + '.' + quotename(so.name)) as quoted_name
      , so.parent_object_id
      , so.[type] as type_code
      , so.type_desc
@@ -16,15 +17,15 @@ select so.object_id
      , so.is_ms_shipped
      , case
            when so.name in (
-		        'fn_diagramobjects'
-		        ,'sp_alterdiagram'
-		        ,'sp_creatediagram'
-		        ,'sp_dropdiagram'
-		        ,'sp_helpdiagramdefinition'
-		        ,'sp_helpdiagrams'
-		        ,'sp_renamediagram'
-		        ,'sp_upgraddiagrams'
-		        ,'sysdiagrams'
+                'fn_diagramobjects'
+                ,'sp_alterdiagram'
+                ,'sp_creatediagram'
+                ,'sp_dropdiagram'
+                ,'sp_helpdiagramdefinition'
+                ,'sp_helpdiagrams'
+                ,'sp_renamediagram'
+                ,'sp_upgraddiagrams'
+                ,'sysdiagrams'
            ) then 1
         when so.is_ms_shipped = 1 then 1
         else 0
